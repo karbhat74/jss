082365fc3c0e9f5d60987ad952631b1ec222d5c1
Karthik A S <160533148+karbhat74@users.noreply.github.com>
2024-12-20 01:51:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,13 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+// Allow requests from your frontend
+app.use(cors({
+    origin: ["http://localhost:5000", "https://jss-5.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
 // MongoDB Connection
 mongoose.connect('mongodb://127.0.0.1:27017/sports-website', {
     useNewUrlParser: true,
